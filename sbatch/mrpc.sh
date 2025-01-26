190f6d061123e6f4d6a37cb95667b02ca074c503
ckp908 <ckp908@usask.ca>
2025-01-26 14:45:36 -0600

    Higher ram for mrpc just in case

--- sbatch/mrpc.sh
+++ sbatch/mrpc.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=3
 #SBATCH --gpus=2           # 
-#SBATCH --mem=8000M
+#SBATCH --mem=24000M
 #SBATCH --time=1-00:00
 #SBATCH --chdir=/scratch/sdmuhsin/Modular-Training
 #SBATCH --output=mrpc-%N-%j.out
