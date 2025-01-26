8734f639cd6a9419dbf62973c8f25fd709a81374
ckp908 <ckp908@usask.ca>
2025-01-26 13:58:16 -0600

    Sbatch fix rte mrpc

--- sbatch/mrpc.sh
+++ sbatch/mrpc.sh
@@ -7,7 +7,7 @@
 #SBATCH --mem=8000M
 #SBATCH --time=1-00:00
 #SBATCH --chdir=/scratch/sdmuhsin/Modular-Training
-#SBATCH --output=rte-%N-%j.out
+#SBATCH --output=mrpc-%N-%j.out
 
 module load python/3.10
 module load arrow/16.1.0
@@ -18,7 +18,6 @@ which python3
 
 export PYTHONPATH="$PYTHONPATH:$(pwd)"
 
-./scripts/modular_pipeline.sh modroberta-rte-m200Aug3xMult1x 200 y y y y 2 y 3 rte 1
-
+./scripts/modular_pipeline.sh modroberta-mprc-m200Aug3xMult1x 200 y y y y 2 y 3 mrpc 1
 
 echo "Run complete"
