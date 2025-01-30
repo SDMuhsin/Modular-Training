0b6e700f33a8360e129134efc172b0d84b1b777b
ckp908 <ckp908@usask.ca>
2025-01-30 09:13:36 -0600

    32GB is a bit much

--- sbatch/rte.sh
+++ sbatch/rte.sh
@@ -4,7 +4,7 @@
 #SBATCH --ntasks-per-node=1
 #SBATCH --cpus-per-task=3
 #SBATCH --gpus=2           # 
-#SBATCH --mem=32000M
+#SBATCH --mem=16000M
 #SBATCH --time=4-00:00
 #SBATCH --chdir=/scratch/sdmuhsin/Modular-Training
 #SBATCH --output=mhainit-rte-%N-%j.out
