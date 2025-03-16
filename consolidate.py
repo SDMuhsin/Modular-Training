1548e9e5bed85ba2d8bcf3d0a18b8948dc349e38
ckp908 <ckp908@usask.ca>
2025-03-16 16:00:13 -0600

    self exp

--- consolidate.py
+++ consolidate.py
@@ -2,7 +2,7 @@ from tabulate import tabulate
 import json 
 
 # Loop through datasets
-tasks = ["boolq","cb","rte","wic","wsc","stsb","mrpc"] #+ ["copa","rte","mrpc","stsb"]
+tasks = ["cb","rte","wic","wsc","stsb","mrpc"] #+ ["copa","rte","mrpc","stsb"]
 seeds = [41,42,43,44,45]
 models = ["roberta-base"]#["bert-base-uncased", "google/mobilebert-uncased", "distilbert-base-uncased", "moddistilbert-base-uncased", "albert-base-v2","t5-small","squeezebert/squeezebert-uncased","microsoft/deberta-v3-xsmall" ]
 # , "huawei-noah/TinyBERT_General_6L_768D"
